911a21794947c9c81ffc8435a67be1e2c460d16e
Tracy Ogutu <tracyogutu@gmail.com>
2020-09-23 02:04:47 +0300

    add admin and agency urls to project

--- travel/urls.py
+++ travel/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    # path('agency/',('agency.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'',include('agency.urls')),
 ]
+
