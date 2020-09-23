71d512748e81b5a7bb79ed94c3ad5d988b6595ae
Tracy Ogutu <tracyogutu@gmail.com>
2020-09-23 03:37:46 +0300

    Display destinations added

--- agency/views.py
+++ agency/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 def welcome(request):
 
-    return render(request, 'welcome.html')
+    destination = Destination.get_all_destinations()
+    return render(request, 'welcome.html',{"destination":destination})
