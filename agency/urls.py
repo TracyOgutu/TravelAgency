a2e973b41053e8c1d2f18dd9ed4fc23cd92c1883
Tracy Ogutu <tracyogutu@gmail.com>
2020-09-28 06:46:26 +0300

    add single_country url

--- agency/urls.py
+++ agency/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url(r'^country/(\d+)',views.single_country,name ='country'),
+    url(r'^destination/(\d+)',views.single_destination,name ='destination'),
+    path('alldest/', views.all_destinations, name='alldest'),
+    
 ]
