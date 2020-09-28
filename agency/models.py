29b707208f3452822193146cefdeb66201509d12
Tracy Ogutu <tracyogutu@gmail.com>
2020-09-28 06:45:57 +0300

    add Country Model

--- agency/models.py
+++ agency/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Customer(models.Model):
-    name = models.CharField(max_length=30)
+    custname = models.CharField(max_length=30)
     email=models.EmailField()
 
     def __str__(self):
@@ -12,19 +12,38 @@ class Customer(models.Model):
     def save_customer(self):
         self.save()
 
+class Country(models.Model):
+    countryname=models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.countryname
+
+    @classmethod
+    def get_all_countries(cls):
+        allcount=cls.objects.all()
+        return allcount
+
+
 
 class Destination(models.Model):
-    name=models.CharField(max_length=30)
+    destincountry=models.ForeignKey(Country,on_delete=models.CASCADE)
+    destname=models.CharField(max_length=30)
     address=models.CharField(max_length=30)
     pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.destname
 
     @classmethod
     def get_all_destinations(cls):
         alldest=cls.objects.all()
         return alldest
 
+
+
+
 class Package(models.Model):
-    name=models.CharField(max_length=30)
+    packagename=models.CharField(max_length=30)
     dest=models.ForeignKey(Destination,on_delete=models.CASCADE)
     price=models.DecimalField(max_digits=6,decimal_places=2)
     description=models.CharField(max_length=100)
