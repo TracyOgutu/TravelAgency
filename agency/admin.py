2c322bec3c56fee02aa69fb4e5caadbb7214ed5b
Tracy Ogutu <tracyogutu@gmail.com>
2020-09-28 06:45:34 +0300

    register country to admin.py

--- agency/admin.py
+++ agency/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Destination,Package
+from .models import Destination,Package,Country
 
 # Register your models here.
+admin.site.register(Country)
 admin.site.register(Destination)
 admin.site.register(Package)
 
