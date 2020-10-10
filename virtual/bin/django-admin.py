c1dca0d11b222c02b79dbc9e3e99eb14b155251e
Tracy Ogutu <tracyogutu@gmail.com>
2020-10-10 23:28:08 +0300

    remove virtual

--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/tracy/Documents/Strathmore/Projects/softeng/virtual/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
