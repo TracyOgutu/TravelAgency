90e7172e3f5bdcd7bb9f6f8c7ea3d0f66296f547
Tracy Ogutu <tracyogutu@gmail.com>
2020-10-10 22:56:16 +0300

    add logout and profile urls

--- agency/urls.py
+++ agency/urls.py
@@ -2,10 +2,15 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 
+
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url(r'^country/(\d+)',views.single_country,name ='country'),
     url(r'^destination/(\d+)',views.single_destination,name ='destination'),
     path('alldest/', views.all_destinations, name='alldest'),
+    url(r'^logout/$',views.logout_function,name="logout"),
+    url(r'^new/profile$', views.new_profile, name='new_profile'),
+    url(r'displayprofile/(\d+)',views.display_profile,name='displayprofile'),
+   
     
 ]
