d2985e1954d24e2e9c84e389658c3f652b41da39
Tracy Ogutu <tracyogutu@gmail.com>
2020-10-10 22:56:00 +0300

    change customer model to profile

--- agency/models.py
+++ agency/models.py
@@ -1,20 +1,32 @@
 from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Customer(models.Model):
-    custname = models.CharField(max_length=30)
-    email=models.EmailField()
-
+class Profile(models.Model):
+    profile_photo=cloudinary.models.CloudinaryField('image',null=True,blank=True)
+    bio=models.CharField(max_length=100)
+    editor=models.ForeignKey(User,on_delete=models.CASCADE)
+    
     def __str__(self):
-        return self.name
+        return self.editor.username
 
-    def save_customer(self):
+    def save_profile(self):
         self.save()
+        
+    @classmethod
+    def single_profile(cls,user_id):
+        '''
+        function gets a single profile posted by id
+        '''
+        profile=cls.objects.get(editor=user_id)
+        return profile
 
 class Country(models.Model):
     countryname=models.CharField(max_length=30)
-    
+    country_image=cloudinary.models.CloudinaryField('image',null=True,blank=True)
     def __str__(self):
         return self.countryname
 
@@ -24,12 +36,12 @@ class Country(models.Model):
         return allcount
 
 
-
 class Destination(models.Model):
     destincountry=models.ForeignKey(Country,on_delete=models.CASCADE)
     destname=models.CharField(max_length=30)
     address=models.CharField(max_length=30)
     pub_date = models.DateTimeField(auto_now_add=True)
+    destination_image =cloudinary.models.CloudinaryField('image',null=True,blank=True)
 
     def __str__(self):
         return self.destname
@@ -40,8 +52,6 @@ class Destination(models.Model):
         return alldest
 
 
-
-
 class Package(models.Model):
     packagename=models.CharField(max_length=30)
     dest=models.ForeignKey(Destination,on_delete=models.CASCADE)
@@ -50,9 +60,11 @@ class Package(models.Model):
     duration=models.IntegerField(default=1)
 
 class Order(models.Model):
-    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    customer=models.ForeignKey(Profile,on_delete=models.CASCADE)
     destination=models.ForeignKey(Destination,on_delete=models.CASCADE)
     package=models.ForeignKey(Package,on_delete=models.CASCADE)
+
+
 
 
 
